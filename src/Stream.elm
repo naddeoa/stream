948a4700c07ed7ed828ec320673379fa491e9199
Anthony Naddeo <anthony.naddeo@gmail.com>
2016-12-22 06:55:43 -0800

    Add most missing documentation
    Should be able to publish after adding more unit tests. There are a few
    cases where I'm not confident about the values I'm returning. I already
    had to update fibonocci because I had mistakenly started at 1 instead of
    0, 1.

--- src/Stream.elm
+++ src/Stream.elm
@@ -14,28 +14,68 @@ module Stream
         , empty
         )
 
-{-|
+{-| A `Stream` is kind of like a stream in Java 8 and kind of like a lazy list. It is a
+potentially infinite stream of items that can be transformed and collected back into
+other structures.
 
-    TODO add append
+This library was made because Elm currently doesn't have great options for
+lazyiness. There are a few implementations that exist but they are vulnerable
+to stack overflows. This library uses tail call recursion for collecting
+streams into lists. Also, instead of modeling the streams recursively it uses
+an approach closer to the Elm architecture. The `Lazy` core library is not
+used. The deepest stack you will have is equal to the amount of transformations
+you apply on a stream.  For example, in the following snippet:
 
-    Unlike Java, can re-use streams after modifying them
+    let
+        finalStream =
+            Stream.fibonocci
+                |> Stream.limit 100
+                |> Stream.filter isEven
+                |> Stream.map toString
 
-    Unlike lazy list, won't stack overflow
+Every time `next` is called on `finalStream` a stack of size three is generated
+before a value can be returned. It scales with the number of transforms, not
+the size of the list. You would have to apply a high enough number of
+transforms to a stream in order to get a stack overflow. That number is
+high enough that you won't have to worry about it. That said, in other
+libraries when you attempt to turn an infinite stream into a list you get a
+stack overflow. In this library, it will just run forever, so, keep that in
+mind.
 
-    # Types
-    @docs Stream
+One useful difference from Java 8 streams that we get just for being functional is
+stream reuse. Things like the following are possible now.
 
-    # Operations on streams
-    @docs limit, map, reduce, filter
+    let
+        baseStream =
+            Stream.naturalNumbers
+                |> Stream.limit 10
 
-    # Getting things out of streams
-    @docs next, nextN, toList
+        firstList =
+            Stream.toList baseStream
 
-    # Creating streams
-    #docs fromList
+        list =
+            baseStream
+                |> Stream.map toString
+                |> Stream.toList
 
-    # Special streams
-    @docs fibonocci, naturalNumbers, empty
+This is obvious to anyone who feeld comfortable with functional programming, but if
+you're coming from Java 8 then you're aware that you can't reuse streams
+that have been collected.
+
+# Types
+@docs Stream
+
+# Operations on streams
+@docs limit, map, reduce, filter
+
+# Getting things out of streams
+@docs next, nextN, toList
+
+# Creating streams
+@docs fromList
+
+# Special streams
+@docs fibonocci, naturalNumbers, empty
 -}
 
 import Stream.Source as Source exposing (Source)
@@ -143,10 +183,13 @@ is `Nothing` then the stream is empty.
 next : Stream a b -> ( Stream a b, Maybe b )
 next stream =
     case stream of
+        Empty ->
+            ( stream, Nothing )
+
         Stream source ->
             let
                 ( nextSource, nextValue ) =
-                    Source.take source
+                    Source.next source
             in
                 ( Stream nextSource, Just <| Source.current source )
 
--- src/Stream/Source.elm
+++ src/Stream/Source.elm
@@ -1,16 +1,33 @@
 module Stream.Source
     exposing
         ( Source
-        , take
-        , taken
+        , next
+        , nextN
         , peek
-        , singleton
+        , current
+        , value
         , naturalNumbers
         , fibonocci
-        , current
         )
 
+{-|
+This module exists mostly to support infinite streams in the `Stream` module.
 
+@docs Source
+
+# Getting things out
+@docs next, nextN, peek, current
+
+# Special sources
+@docs value, naturalNumbers, fibonocci
+-}
+
+
+{-| The main Source type.
+As a rule, all `Source`s are infinite. They are really just a way to
+model the idea of a starting element and a function to generate the
+next element.
+-}
 type Source a
     = Source a (a -> a)
 
@@ -19,13 +36,28 @@ type Source a
 -- Operations for getting stuff out of sources
 
 
-take : Source a -> ( Source a, a )
-take source =
+{-| Get the next value out of a `Source`, as well as a new
+source that will return the next value after.
+
+    -- 1
+    Source.naturalNumbers
+        |> Source.next
+        |> Tuple.second
+-}
+next : Source a -> ( Source a, a )
+next source =
     ( nextSource source, peek source )
 
 
-taken : Int -> Source a -> ( Source a, List a )
-taken n source =
+{-| Like `next` but it returns a list of `n` items instead.
+
+    -- [ 1, 2, 3 ]
+    Source.naturalNumbers
+        |> Source.nextN 3
+        |> Tuple.second
+-}
+nextN : Int -> Source a -> ( Source a, List a )
+nextN n source =
     takenHelper n source []
 
 
@@ -37,44 +69,68 @@ takenHelper n source acc =
         takenHelper (n - 1) (nextSource source) ((peek source) :: acc)
 
 
+{-| Get the next value from a source without the next `Source` state.
+TODO do we need peek?  maybe curent is enough
+
+    -- 2
+    Source.naturalNumbers
+        |> Source.peek
+-}
 peek : Source a -> a
-peek (Source a next) =
-    next a
+peek (Source a f) =
+    f a
 
 
+{-| Get the current value from a source. Kind of like `head` for a `Source`.
+
+    -- 1
+    Source.naturalNumbers
+        |> Source.current
+-}
 current : Source a -> a
-current (Source a next) =
+current (Source a _) =
     a
 
 
+{-| Get the next state of a `Source` such that  calling `current` on that result
+will return the next value that you would expect.
+
+    -- 1
+    Source.naturalNumbers
+        |> Source.current
+
+    -- 2
+    Source.nextSource
+        |> Source.naturalNumbers
+        |> Source.current
+-}
 nextSource : Source a -> Source a
 nextSource (Source a next) =
     Source (next a) next
 
 
 
--- Utility functions
-
-
-identity : a -> a
-identity a =
-    a
-
-
-
 -- Special sources
 
 
-singleton : a -> Source a
-singleton a =
-    Source a identity
+{-| A `Source` that returns the same thing forever.
+-}
+value : a -> Source a
+value a =
+    Source a (\a -> a)
 
 
+{-| A source of natural numbers from 1 to infinity.
+-}
 naturalNumbers : Source Int
 naturalNumbers =
     Source 1 ((+) 1)
 
 
+{-| A source of tuples that represent the fibonocci numbers. This
+is used by the `Stream` module and presented as values instead of tuples, but
+this `Source` is what enables that.
+-}
 fibonocci : Source ( Int, Int )
 fibonocci =
     Source ( 0, 1 ) (\( a, b ) -> ( b, a + b ))
